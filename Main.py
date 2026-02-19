c002ed42141455def5ed4d7e7bc7b7de10a8c901
Dmitrii Kerimov <5555kdi@mail.ru>
2026-02-19 19:17:17 +0300

    Initial commit

---
+++ Main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    t = request.args.get('title', 'Главная страница')
+    return render_template('base.html', title=t)
+
+if __name__ == '__main__':
+    app.run(port=8080)
